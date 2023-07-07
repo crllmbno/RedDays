262dfff5836c30062edd64675fd1efde416be01b
Margallo <acelmargallo04@gmail.com>
2023-07-08 01:39:27 +0800

    Spinner and RadioButton

--- app/src/main/java/com/example/periodcheck/Question1.kt
+++ app/src/main/java/com/example/periodcheck/Question1.kt
@@ -2,12 +2,36 @@ package com.example.periodcheck
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class Question1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question1)
 
+        val itemList = listOf(
+            "1 day",
+            "2 days",
+            "3 days",
+            "4 days",
+            "5 days",
+            "6 days",
+            "7 days",
+            "More than 7 days"
+        )
+        val spinner = findViewById<Spinner>(R.id.spinner)
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, itemList)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spinner.adapter = adapter
 
     }
 }
+
+
+
+
