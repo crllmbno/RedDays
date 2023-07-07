726fbb498dae51f27a18bb5702164bd1165f9580
Margallo <acelmargallo04@gmail.com>
2023-07-07 20:12:58 +0800

    navigation

--- app/src/main/java/com/example/periodcheck/SplashScreen.kt
+++ app/src/main/java/com/example/periodcheck/SplashScreen.kt
@@ -1,23 +1,44 @@
 package com.example.periodcheck
 
+
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 
+
 class SplashScreen : AppCompatActivity() {
+
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
         supportActionBar?.hide()
 
-        val handler = Handler(Looper.getMainLooper())
-        handler.postDelayed({
-            val intent = Intent(this,MainActivity::class.java)
+        val SPLASH_DELAY: Long = 3000 // Delay time in milliseconds (3 seconds)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Proceed to the onboarding screen (OnboardingActivity)
+            val intent = Intent(this, Welcome::class.java)
             startActivity(intent)
             finish()
-        },3000)
+        }, SPLASH_DELAY)
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/periodcheck/Welcome.kt
@@ -0,0 +1,35 @@
+package com.example.periodcheck
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Context
+import android.content.Intent
+
+
+class Welcome : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+
+            val sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+            val isFirstTime = sharedPreferences.getBoolean("isFirstTime", true)
+
+            if (isFirstTime) {
+                // Show the onboarding content
+                setContentView(R.layout.activity_welcome)
+
+                // Update the shared preferences to indicate that onboarding has been shown
+                val editor = sharedPreferences.edit()
+                editor.putBoolean("isFirstTime", false)
+                editor.apply()
+            } else {
+                // Proceed to the next screen (e.g., MainActivity)
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+    }
+
+
+
+
