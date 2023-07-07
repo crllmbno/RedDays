4e8fdb8da43be3782024136fe99c47e4b03e3919
Margallo <acelmargallo04@gmail.com>
2023-07-07 23:14:52 +0800

    questions UI

---
+++ app/src/main/java/com/example/periodcheck/Question1.kt
@@ -0,0 +1,13 @@
+package com.example.periodcheck
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Question1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question1)
+
+
+    }
+}
--- app/src/main/java/com/example/periodcheck/Welcome.kt
+++ app/src/main/java/com/example/periodcheck/Welcome.kt
@@ -4,31 +4,41 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Context
 import android.content.Intent
+import android.widget.ImageButton
 
 
 class Welcome : AppCompatActivity() {
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-            val sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
-            val isFirstTime = sharedPreferences.getBoolean("isFirstTime", true)
+        val sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+        val isFirstTime = sharedPreferences.getBoolean("isFirstTime", true)
 
-            if (isFirstTime) {
-                // Show the onboarding content
-                setContentView(R.layout.activity_welcome)
+        if (isFirstTime) {
+            // Show the onboarding content
+            setContentView(R.layout.activity_welcome)
 
-                // Update the shared preferences to indicate that onboarding has been shown
-                val editor = sharedPreferences.edit()
-                editor.putBoolean("isFirstTime", false)
-                editor.apply()
-            } else {
-                // Proceed to the next screen (e.g., MainActivity)
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-                finish()
-            }
+            // Update the shared preferences to indicate that onboarding has been shown
+            val editor = sharedPreferences.edit()
+            editor.putBoolean("isFirstTime", false)
+            editor.apply()
+        } else {
+            // Proceed to the next screen (e.g., MainActivity)
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
         }
+
+        val myImageButton = findViewById<ImageButton>(R.id.btnNext)
+
+        myImageButton.setOnClickListener {
+            val intent = Intent(this, Question1::class.java)
+            startActivity(intent)
+        }
+
     }
+}
+
 
 
 
